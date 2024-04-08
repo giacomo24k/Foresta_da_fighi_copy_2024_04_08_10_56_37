d7a9f5770e51c03f96daa8f3fb27e9290d0db031
your name <your_email@example.com>
2024-04-08 13:10:15 +0200

    first commit

---
+++ sketch.js
@@ -0,0 +1,46 @@
+/**
+
+  Duplicate this sketch to start your project 🧪
+  More info: https://dev.codemotionkids.com/libraries/diorama/docs/
+  
+  Creative Commons BY-SA
+
+**/
+
+function setup() {
+  createCanvas3D(600, 400);
+  hideGrid();
+  //Change background color of the scene
+
+  environment(FOREST);
+  background3D("white"); 
+
+  let albero = beginGroup();
+  diffuse(3,226,3);
+  // sphere(0, 3, 0, 2);
+  align(TOP);
+  capsule(0, 4, 0, 2.5, 4);
+  diffuse(139,25,25);
+  cylinder(0, 0, 0, 0.8, 5);
+  endGroup();
+  
+  let lato = 100;
+  align(BOTTOM);
+  diffuse("#27800A")
+  
+  for (let i = 0; i < 100; i++) {
+    let x = random(-lato/2,lato/2);
+    let z = random(-lato/2,lato/2);
+    let nuovoAlbero = clone(albero,x,0,z);       
+    let scala = random(1,2);
+    nuovoAlbero.setScale(scala);
+  }
+
+  
+  
+}
+
+function draw() {
+  // 🔍 Never create objects here, but don't forget to have it in your code:
+  // leave empty for the moment
+}
